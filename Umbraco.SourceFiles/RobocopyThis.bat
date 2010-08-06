39f2fadcdc51c55e54c8598fa61891d9a655c62e
Geert Pasteels <geert.pasteels@gmail.com>
2010-08-06 13:33:07 +0200

    added pulling from umbraco. Check the updated README

--- Umbraco.SourceFiles/RobocopyThis.bat
+++ Umbraco.SourceFiles/RobocopyThis.bat
@@ -18,21 +18,28 @@ ROBOCOPY %sourcedir%bin %targetdir%bin /XO
 
 ::CSS (include subfolders)
 ROBOCOPY %sourcedir%css %targetdir%css /MIR /XO /E
+ROBOCOPY %targetdir%css %sourcedir%css /XO /E
 
 ::IMAGES (include subfolders)
 ROBOCOPY %sourcedir%images %targetdir%images /MIR /XO /E
+ROBOCOPY %targetdir%images %sourcedir%images /XO /E
 
 ::PYTHON (include subfolders)
 ROBOCOPY %sourcedir%python %targetdir%python /MIR /XO /E
+ROBOCOPY %targetdir%python %sourcedir%python /XO /E
 
 ::SCRIPTS (include subfolders)
-ROBOCOPY %sourcedir%scripts %targetdir%scripts /MIR /XO /E
+ROBOCOPY %targetdir%scripts %sourcedir%scripts /MIR /XO /E
+ROBOCOPY %sourcedir%scripts %targetdir%scripts /XO /E
 
 ::MASTERPAGES
 ROBOCOPY %sourcedir%masterpages %targetdir%masterpages *.master /MIR /XO
+ROBOCOPY %targetdir%masterpages %sourcedir%masterpages *.master /XO
 
 ::USERCONTROLS (should only copy the *.ascx files)
 ROBOCOPY %sourcedir%usercontrols %targetdir%usercontrols *.ascx /MIR /XO
+ROBOCOPY %targetdir%usercontrols %sourcedir%usercontrols *.ascx /XO
 
 ::XSLT
 ROBOCOPY %sourcedir%xslt %targetdir%xslt /MIR /XO
+ROBOCOPY %targetdir%xslt %sourcedir%xslt /MIR /XO
